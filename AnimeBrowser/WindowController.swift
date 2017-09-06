9b6b33191d8881aa92bcd5c2c854ec5b6e82ebca
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-09-05 23:03:07 -0400

    At a very fundamental level, this counts as a functional browser

--- AnimeBrowser/WindowController.swift
+++ AnimeBrowser/WindowController.swift
@@ -9,11 +9,106 @@
 import Cocoa
 
 class WindowController: NSWindowController {
+    @IBOutlet weak var toolBar: NSToolbar!
 
+    @IBOutlet var browserHeaderView: NSView!
+    
+    @IBOutlet weak var urlField: NSTextField!
+    
+    let BrowserHeaderToolbarID = "BrowserHeaderID"
+    
     override func windowDidLoad() {
         super.windowDidLoad()
 
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        self.toolBar.allowsUserCustomization = true
+        self.toolBar.autosavesConfiguration = true
+        self.toolBar.displayMode = .iconOnly
     }
     
+    @IBAction func submitURLString(_ sender: NSTextField) {
+        let url = URL(string: sender.stringValue)
+        if let vc = self.window?.contentViewController as? ViewController{
+            let req = URLRequest(url: url!)
+            vc.mainWebView.load(req)
+        }
+    }
+    
+    func customToolbarItem(itemForItemIdentifier itemIdentifier: String, label: String, paletteLabel: String, toolTip: String, target: AnyObject, itemContent: AnyObject, action: Selector?, menu: NSMenu?) -> NSToolbarItem? {
+        
+        let toolbarItem = NSToolbarItem(itemIdentifier: itemIdentifier)
+        
+        toolbarItem.label = label
+        toolbarItem.paletteLabel = paletteLabel
+        toolbarItem.toolTip = toolTip
+        toolbarItem.target = target
+        toolbarItem.action = action
+        
+        // Set the right attribute, depending on if we were given an image or a view.
+        if (itemContent is NSImage) {
+            let image: NSImage = itemContent as! NSImage
+            toolbarItem.image = image
+        }
+        else if (itemContent is NSView) {
+            let view: NSView = itemContent as! NSView
+            toolbarItem.view = view
+        }
+        else {
+            assertionFailure("Invalid itemContent: object")
+        }
+        
+        /* If this NSToolbarItem is supposed to have a menu "form representation" associated with it
+         (for text-only mode), we set it up here.  Actually, you have to hand an NSMenuItem
+         (not a complete NSMenu) to the toolbar item, so we create a dummy NSMenuItem that has our real
+         menu as a submenu.
+         */
+        // We actually need an NSMenuItem here, so we construct one.
+        let menuItem: NSMenuItem = NSMenuItem()
+        menuItem.submenu = menu
+        menuItem.title = label
+        toolbarItem.menuFormRepresentation = menuItem
+        
+        return toolbarItem
+    }
+
+    
+}
+
+extension WindowController:NSToolbarDelegate {
+    /**
+     NSToolbar delegates require this function.
+     It takes an identifier, and returns the matching NSToolbarItem. It also takes a parameter telling
+     whether this toolbar item is going into an actual toolbar, or whether it's going to be displayed
+     in a customization palette.
+     */
+    func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: String, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        
+        var toolbarItem: NSToolbarItem = NSToolbarItem()
+        
+        /* We create a new NSToolbarItem, and then go through the process of setting up its
+         attributes from the master toolbar item matching that identifier in our dictionary of items.
+         */
+        if (itemIdentifier == BrowserHeaderToolbarID) {
+            // 1) Font style toolbar item.
+            toolbarItem = customToolbarItem(itemForItemIdentifier: BrowserHeaderToolbarID, label: "Browser Header", paletteLabel:"Browser header", toolTip: "Browser header", target: self, itemContent: self.browserHeaderView, action: nil, menu: nil)!
+        }
+        
+        return toolbarItem
+    }
+    
+    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [String] {
+        
+        return [BrowserHeaderToolbarID]
+
+    }
+    
+    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [String] {
+        
+        return [ BrowserHeaderToolbarID,
+                 NSToolbarSpaceItemIdentifier,
+                 NSToolbarFlexibleSpaceItemIdentifier,
+                 NSToolbarPrintItemIdentifier ]
+    }
+    
+
+
 }
